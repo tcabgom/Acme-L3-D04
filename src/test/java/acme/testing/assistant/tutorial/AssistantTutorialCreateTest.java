e529fe7505437054e7b310edf0d036761e0c1d98
tcabgom <tcabgom222@gmail.com>
2023-04-27 17:53:28 +0200

    More progress

--- src/test/java/acme/testing/assistant/tutorial/AssistantTutorialCreateTest.java
+++ src/test/java/acme/testing/assistant/tutorial/AssistantTutorialCreateTest.java
@@ -11,18 +11,49 @@ public class AssistantTutorialCreateTest extends TestHarness {
 
 	@ParameterizedTest
 	@CsvFileSource(resources = "/assistant/tutorial/create-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-	public void test100Positive(final int recordIndex) {
+	public void test100Positive(final int recordIndex, final String code, final String title, final String course, final String tutorialAbstract, final String goals, final String estimatedTotalTime, final String numberOfSessions, final String draftMode,
+		final String courseTitle) {
 		// HINT: this test authenticates as an assistant and then lists his or her
 		// HINT: tutorials, creates a new one, and check that it's been created properly.
 
 		super.signIn("assistant1", "assistant1");
 
 		super.clickOnMenu("Assistant", "My Tutorials");
+		super.clickOnButton("Create");
 		super.checkFormExists();
 
 		super.clickOnButton("Create");
-
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("course", course);
+		super.fillInputBoxIn("code", code);
+		super.fillInputBoxIn("tutorialAbstract", tutorialAbstract);
+		super.fillInputBoxIn("goals", goals);
+		super.fillInputBoxIn("estimatedTotalTime", estimatedTotalTime);
+		super.fillInputBoxIn("numberOfSessions", numberOfSessions);
+		super.fillInputBoxIn("draftMode", draftMode);
 		super.clickOnSubmit("Create");
+
+		super.clickOnMenu("Assistant", "My Tutorials");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.checkColumnHasValue(recordIndex, 0, title);
+		super.checkColumnHasValue(recordIndex, 1, draftMode);
+		super.checkColumnHasValue(recordIndex, 2, courseTitle);
+		super.checkColumnHasValue(recordIndex, 3, numberOfSessions);
+		super.clickOnListingRecord(recordIndex);
+
+		super.checkFormExists();
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("course", course);
+		super.checkInputBoxHasValue("code", code);
+		super.checkInputBoxHasValue("tutorialAbstract", tutorialAbstract);
+		super.checkInputBoxHasValue("goals", goals);
+		super.checkInputBoxHasValue("estimatedTotalTime", estimatedTotalTime);
+		super.checkInputBoxHasValue("numberOfSessions", numberOfSessions);
+		super.checkInputBoxHasValue("draftMode", draftMode);
+
+		super.clickOnButton("Manage Sessions");
 
 		super.checkListingExists();
 		super.checkListingEmpty();
@@ -33,9 +64,30 @@ public class AssistantTutorialCreateTest extends TestHarness {
 
 	@ParameterizedTest
 	@CsvFileSource(resources = "/assistant/tutorial/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
-	public void test200Negative(final int recordIndex) {
+	public void test200Negative(final int recordIndex, final String code, final String title, final String course, final String tutorialAbstract, final String goals, final String estimatedTotalTime, final String numberOfSessions, final String draftMode) {
 		// HINT: this test authenticates as an assistant and then lists his or her
 		// HINT: tutorials, creates a new one, and check that it's been created properly.
+
+		super.signIn("assistant1", "assistant1");
+
+		super.clickOnMenu("Assistant", "My Tutorials");
+		super.clickOnButton("Create");
+		super.checkFormExists();
+
+		super.clickOnButton("Create");
+		super.fillInputBoxIn("title", title);
+		super.fillInputBoxIn("course", course);
+		super.fillInputBoxIn("code", code);
+		super.fillInputBoxIn("tutorialAbstract", tutorialAbstract);
+		super.fillInputBoxIn("goals", goals);
+		super.fillInputBoxIn("estimatedTotalTime", estimatedTotalTime);
+		super.fillInputBoxIn("numberOfSessions", numberOfSessions);
+		super.fillInputBoxIn("draftMode", draftMode);
+		super.clickOnSubmit("Create");
+
+		super.checkErrorsExist();
+
+		super.signOut();
 
 	}
 
---
+++ src/test/java/acme/testing/assistant/tutorial/AssistantTutorialPublishTest.java
@@ -0,0 +1,17 @@
+
+package acme.testing.assistant.tutorial;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.testing.TestHarness;
+
+public class AssistantTutorialPublishTest extends TestHarness {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected AssistantTutorialTestRepository repository;
+
+	// Test data --------------------------------------------------------------
+
+}
