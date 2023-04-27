1110e2065ef6bc171e8b264fda45f2269ab7858e
narromant <ignarrman@alum.us.es>
2023-04-27 17:41:37 +0200

    Audit test finished

--- src/main/java/acme/components/BannerRepository.java
+++ src/main/java/acme/components/BannerRepository.java
@@ -4,6 +4,7 @@ package acme.components;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.Query;
@@ -20,7 +21,7 @@ public interface BannerRepository extends AbstractRepository {
 	int countBanners(Date currentMoment);
 
 	@Query(value = "SELECT b FROM Banner b")
-	List<Banner> findAllBanners(PageRequest pageRequest);
+	List<Banner> findAllBanners();
 
 	default Banner findRandomBanner() {
 		Banner result;
@@ -34,10 +35,12 @@ public interface BannerRepository extends AbstractRepository {
 			result = null;
 		else {
 			random = ThreadLocalRandom.current();
+			final Date currentMoment = MomentHelper.getCurrentMoment();
 			index = random.nextInt(0, count);
 			page = PageRequest.of(index, 1);
-			list = this.findAllBanners(page);
-			result = list.isEmpty() ? null : list.get(0);
+			list = this.findAllBanners().stream().filter(b -> MomentHelper.isBefore(b.getDisplayPeriodInitial(), currentMoment) && MomentHelper.isAfter(b.getDisplayPeriodEnding(), currentMoment)).collect(Collectors.toList());
+			;
+			result = list.isEmpty() ? null : list.get(index);
 		}
 
 		return result;
--- src/test/java/acme/testing/auditor/audit/AuditorAuditPublishTest.java
+++ src/test/java/acme/testing/auditor/audit/AuditorAuditPublishTest.java
@@ -1,6 +1,125 @@
+
 package acme.testing.auditor.audit;
 
+import java.util.Collection;
 
-public class AuditorAuditPublishTest {
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.entities.audit.Audit;
+import acme.testing.TestHarness;
+
+public class AuditorAuditPublishTest extends TestHarness {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected AuditorAuditTestRepository repository;
+
+	// Test methods ------------------------------------------------------------
+
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/auditor/audit/publish-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int recordIndex, final String code) {
+		// HINT: this test logs in as an employer, lists his or her jobs, 
+		// HINT+ selects one of them, updates it, and then checks that 
+		// HINT+ the update has actually been performed.
+
+		super.signIn("auditor1", "auditor1");
+
+		super.clickOnMenu("Auditor", "Manage your Audits");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+		super.checkColumnHasValue(recordIndex, 0, code);
+
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		super.clickOnSubmit("Publish");
+		super.checkNotErrorsExist();
+
+		super.signOut();
+	}
+
+	@ParameterizedTest
+	@CsvFileSource(resources = "/auditor/audit/publish-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test200Negative(final int recordIndex, final String code) {
+		// HINT: this test attempts to update a job with wrong data.
+
+		super.signIn("auditor1", "auditor1");
+
+		super.clickOnMenu("Auditor", "Manage your Audits");
+		super.checkListingExists();
+		super.sortListing(0, "asc");
+
+		super.checkColumnHasValue(recordIndex, 0, code);
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		super.clickOnSubmit("Publish");
+		super.checkAlertExists(false);
+
+		super.signOut();
+	}
+
+	@Test
+	public void test300Hacking() {
+		// HINT: this test tries to update a job with a role other than "Employer",
+		// HINT+ or using an employer who is not the owner.
+
+		Collection<Audit> audits;
+		String param;
+
+		audits = this.repository.findMyAuditsByAuditorUsername("auditor1");
+		for (final Audit audit : audits) {
+			param = String.format("id=%d", audit.getId());
+
+			super.checkLinkExists("Sign in");
+			super.request("/auditor/audit/publish", param);
+			super.checkPanicExists();
+
+			super.signIn("administrator1", "administrator1");
+			super.request("/auditor/audit/publish", param);
+			super.checkPanicExists();
+			super.signOut();
+
+			super.signIn("assistant1", "assistant1");
+			super.request("/auditor/audit/publish", param);
+			super.checkPanicExists();
+			super.signOut();
+		}
+	}
+	@Test
+	public void test301Hacking() {
+		Collection<Audit> audits;
+		String params;
+
+		super.signIn("employer1", "employer1");
+		audits = this.repository.findMyAuditsByAuditorUsername("auditor1");
+		for (final Audit audit : audits)
+			if (!audit.isDraftMode()) {
+				params = String.format("id=%d", audit.getId());
+				super.request("/auditor/audit/publish", params);
+			}
+		super.signOut();
+	}
+
+	@Test
+	public void test302Hacking() {
+		// HINT: this test tries to publish a job that wasn't registered by the principal,
+		// HINT+ be it published or unpublished.
+
+		Collection<Audit> audits;
+		String params;
+
+		super.signIn("auditor2", "auditor2");
+		audits = this.repository.findMyAuditsByAuditorUsername("auditor1");
+		for (final Audit audit : audits) {
+			params = String.format("id=%d", audit.getId());
+			super.request("/auditor/audit/publish", params);
+		}
+		super.signOut();
+	}
 
 }
