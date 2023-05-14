c936d306ab8d972a6cfb42e25ed59ee83e9a28b9
tcabgom <tcabgom222@gmail.com>
2023-05-14 18:03:19 +0200

    Added more test data and improved previous data

--- src/test/java/acme/testing/assistant/tutorial/AssistantTutorialPublishTest.java
+++ src/test/java/acme/testing/assistant/tutorial/AssistantTutorialPublishTest.java
@@ -1,10 +1,14 @@
 
 package acme.testing.assistant.tutorial;
 
+import java.util.Collection;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import acme.entities.tutorial.Tutorial;
 import acme.testing.TestHarness;
 
 public class AssistantTutorialPublishTest extends TestHarness {
@@ -55,6 +59,71 @@ public class AssistantTutorialPublishTest extends TestHarness {
 		super.clickOnSubmit("Publish");
 		super.checkAlertExists(false);
 
+		super.signOut();
+	}
+
+	@Test
+	public void test300Hacking() {
+		// HINT: this test tries to publish a tutorial with a role other than "Assistant".
+
+		Collection<Tutorial> tutorials;
+		String param;
+
+		tutorials = this.repository.findManyTutorialsByAssistantUsername("assistant1");
+		for (final Tutorial t : tutorials)
+			if (t.isDraftMode()) {
+
+				param = String.format("id=%d", t.getId());
+
+				super.checkLinkExists("Sign in");
+				super.request("/assistant/tutorial/publish", param);
+				super.checkPanicExists();
+
+				super.signIn("administrator1", "administrator1");
+				super.request("/assistant/tutorial/publish", param);
+				super.checkPanicExists();
+				super.signOut();
+
+				super.signIn("lecturer1", "lecturer1");
+				super.request("/assistant/tutorial/publish", param);
+				super.checkPanicExists();
+				super.signOut();
+
+			}
+
+	}
+
+	@Test
+	public void test301Hacking() {
+		// HINT: this test tries to publish a published tutorial that was registered by the principal.
+
+		Collection<Tutorial> tutorials;
+		String param;
+
+		super.signIn("assistant1", "assistant1");
+		tutorials = this.repository.findManyTutorialsByAssistantUsername("assistant1");
+		for (final Tutorial t : tutorials)
+			if (!t.isDraftMode()) {
+				param = String.format("id=%d", t.getId());
+				super.request("/assistant/tutorial/publish", param);
+			}
+		super.signOut();
+	}
+
+	@Test
+	public void test302Hacking() {
+		// HINT: this test tries to publish a tutorial that wasn't registered by the principal,
+		// HINT+ be it published or unpublished.
+
+		Collection<Tutorial> tutorials;
+		String param;
+
+		super.signIn("assistant2", "assistant2");
+		tutorials = this.repository.findManyTutorialsByAssistantUsername("assistant1");
+		for (final Tutorial t : tutorials) {
+			param = String.format("id=%d", t.getId());
+			super.request("/assistant/tutorial/publish", param);
+		}
 		super.signOut();
 	}
 
